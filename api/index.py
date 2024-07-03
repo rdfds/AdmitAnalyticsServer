b7d0bf11d101e4e2a878678055344f20d28ccf57
rvariankaval <98034438+rvariankaval@users.noreply.github.com>
2024-07-03 10:33:31 -0400

    single to double quotes

--- api/index.py
+++ api/index.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World'
+    return "Hello World"
 
 # main driver function
 #if __name__ == '__main__':
