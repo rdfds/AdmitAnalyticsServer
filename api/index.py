57505cc39952aef955334b8677d83ba7cbdc2644
rvariankaval <98034438+rvariankaval@users.noreply.github.com>
2024-07-16 22:04:09 -0400

    trying to test solutions

--- api/index.py
+++ api/index.py
@@ -355,8 +355,8 @@ def find_similar_entries(user_id, interested_colleges, major):
     similar_entries = []
     
     str1 = str(interested_colleges) + " and then " + str(results_data)
-    return str1
-
+    #return str1
+    return str(filtered_post_ids_colleges)
     for post_id in filtered_post_ids:
         entry = compile_entry(post_id, demographics_data, academics_data, majors_data)
         similarity = calculate_similarity(user_info, entry)
