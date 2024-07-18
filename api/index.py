509ec460dd77f4a3a2442c19686f337a16b9b3da
rvariankaval <98034438+rvariankaval@users.noreply.github.com>
2024-07-18 00:56:47 -0400

    fixing errors

--- api/index.py
+++ api/index.py
@@ -392,7 +392,7 @@ def find_similar_entries(user_id, interested_colleges, major):
     filtered_post_ids_majors, count2 = filter_entries_by_major(major, majors_data)
     filtered_post_ids = find_intersection(filtered_post_ids_majors, filtered_post_ids_colleges)
     similar_entries = []
-    return str(len(demographics_data)) + " " + str(demographics_data)
+    #return str(len(demographics_data)) + " " + str(demographics_data)
 
     #return str(filtered_post_ids_majors) + " " + str(count)
     #str1 = str(interested_colleges) + " and then " + str(results_data)
